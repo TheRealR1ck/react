7aeae7939068b7c1d444a5c8d1b7d6bad3a0c01b
Erik <eriksarabia7@gmail.com>
2022-09-12 21:55:09 -0700

    a clean initial structure

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello There Monkey BO!!
+          Hello There Monkey BO!!!
         </p>
         <a
           className="App-link"
